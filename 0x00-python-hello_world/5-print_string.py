ff278463cbab8d469284eff14bff176df15b7b3b
Noble <ruzibizanoble@gmail.com>
2022-06-01 20:10:15 -0700

    My Commit

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Holberton School"
+print(str * 3)
+
+print([0:9])
+© 2022 GitHub, Inc.
---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str1 = "The Zen of Python"
+str2 = ",by TimPeters"
+str = str1 + str2
+print(str)
