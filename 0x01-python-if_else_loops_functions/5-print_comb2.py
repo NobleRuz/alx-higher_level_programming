1314c299f759d38dc71eff6b8edef2bca3d37b23
Noble <ruzibizanoble@gmail.com>
2022-06-14 06:53:14 -0700

    00 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for num in range(0, 100):
-    if num in [99]:
-        print("{:d}".format(num))
-        break
-    print("{:02d}, ".format(num), end="")
+for num in range(00, 100):
+    print("{:02d}".format(num), end='\n' if num == 99 else ", ")
