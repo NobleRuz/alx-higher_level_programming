f79407b424090627cc8a3a970f59c63f5606f104
Noble <ruzibizanoble@gmail.com>
2022-06-21 17:01:29 -0700

    Commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): Size of new square.
+        """
+        self.__size = size
